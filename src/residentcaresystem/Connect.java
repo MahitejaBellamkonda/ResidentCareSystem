9e561e003c2aa2f642dfe523fc2d5c7dfec7c2f5
Mahiteja Bellamkonda <bmahiteja13@gmail>
2021-06-04 19:53:35 +1000

    Commited all the files

---
+++ src/residentcaresystem/Connect.java
@@ -0,0 +1,25 @@
+package residentcaresystem;
+
+/**
+ *
+ * @author Mahiteja Bellamkonda
+ */
+import java.sql.*;
+import javax.swing.*;
+public class Connect {
+     Connection con=null;
+   
+        public static Connection ConnectDB(){
+             try{
+           
+          Class.forName("com.mysql.cj.jdbc.Driver");
+         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hms_db","root","");
+         
+          return con;
+            
+        }catch(ClassNotFoundException | SQLException e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+    }      
+}
+}
